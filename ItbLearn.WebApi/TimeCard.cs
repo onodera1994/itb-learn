54c8031aed0a765b61c7ced74315a653574eccec
onodera1994 <y.onodera1994@gmail.com>
2020-10-15 11:15:15 +0900

    add web api

---
+++ ItbLearn.WebApi/TimeCard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItbLearn.WebApi
+{
+    public class TimeCard
+    {
+        public string UserName { get; set; }
+        public DateTimeOffset Time { get; set; }
+    }
+}
