5f78e26fde487a98668b189eaa3eca36bd4d6fe5
onodera1994 <y.onodera1994@gmail.com>
2020-10-16 11:55:30 +0900

    updata web api to save timecard to databese

--- ItbLearn.WebApi/Controllers/TimeCardsController.cs
+++ ItbLearn.WebApi/Controllers/TimeCardsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ItbLearn.Models;
+using ItbLearn.WebApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -13,9 +14,25 @@ namespace ItbLearn.WebApi.Controllers
     [ApiController]
     public class TimeCardsController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult Post(TimeCard timeCard)
+        private readonly TimeCardsDbContext timeCardsDbContext;
+
+        public TimeCardsController(TimeCardsDbContext timeCardsDbContext)
         {
+            this.timeCardsDbContext = timeCardsDbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(TimeCard timeCard)
+        {
+            TimeCardEntity timeCardEntity = new TimeCardEntity();
+
+            timeCardEntity.Id = $"{timeCard.Time.ToString("yyyy-MM-dd")}-{timeCard.UserName}";
+            timeCardEntity.UserName = timeCard.UserName;
+            timeCardEntity.ArrivedTime = timeCard.Time;
+
+            await timeCardsDbContext.TimeCards.AddAsync(timeCardEntity);
+            
+            await timeCardsDbContext.SaveChangesAsync();
             return Ok("Hello");
         }
      
---
+++ ItbLearn.WebApi/Models/TimeCardEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItbLearn.WebApi.Models
+{
+    public class TimeCardEntity
+    {
+        [Key]
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public DateTimeOffset ArrivedTime { get; set; }
+        public DateTimeOffset? LeftTime { get; set; }
+    }
+}
