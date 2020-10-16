7804f215199f08394ee25eac29656732c8f1c623
onodera1994 <72848716+onodera1994@users.noreply.github.com>
2020-10-16 12:00:40 +0900

    updata web api to save timecard to databese (#2)

---
+++ ItbLearn.WebApi/Models/TimeCardsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItbLearn.WebApi.Models
+{
+    public class TimeCardsDbContext : DbContext
+    {
+        public TimeCardsDbContext(DbContextOptions<TimeCardsDbContext> options) : base(options) { }
+        public DbSet<TimeCardEntity> TimeCards { get; set; }
+
+    }
+}
